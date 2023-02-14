f7fb1063f3e3f8a8ff69e0a18645d25f4b545a40
vidhi jain <117736922+vi3003@users.noreply.github.com>
2023-02-14 21:29:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,18 @@
 //your code here
+let night = document.getElementById("app");
+let button = document.getElementById("swap");
+
+swap.addEventListener("click", function(){
+	if(night.classList.contains("day")){
+		night.classList.remove("day");
+		night.classList.add("night");
+		button.classList.remove("button_day");
+		button.classList.add("button_night");
+	} else {
+		night.classList.remove("night");
+		night.classList.add("day");
+		button.classList.remove("button_night");
+		button.classList.add("button_day");
+	}
+});
 
